6dbd604cca4022861def06862ab0324b0a5f7722
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2022-07-19 22:45:19 -0400

    firebase configured

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAXu3Gn5OxguH1aBBUD8V-h6f6k3R0UWdY",
+    authDomain: "linkedfy-cl.firebaseapp.com",
+    projectId: "linkedfy-cl",
+    storageBucket: "linkedfy-cl.appspot.com",
+    messagingSenderId: "443373251687",
+    appId: "1:443373251687:web:3f0c1467564845e1b578c2"
+  };
